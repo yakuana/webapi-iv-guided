43d46c14d1c4b7045be02f56ffa090f18854d2b6
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-09-05 14:58:21 -0400

    Added production key.

--- knexfile.js
+++ knexfile.js
@@ -18,4 +18,24 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/shouts.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };
